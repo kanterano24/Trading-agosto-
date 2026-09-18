490fc0895bbdc0807d556a80ca78d753299b3e60
kanterano24 <eduardojpc2026@gmail.com>
2026-09-17 20:22:29 -0500

    Bot.py real

--- bot.py
+++ bot.py
@@ -50,13 +50,13 @@ TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 TIMEFRAME = 60
 EXPIRATION = int(os.getenv("EXPIRATION", "1"))
-AMOUNT = float(os.getenv("AMOUNT", "150"))
+AMOUNT = float(os.getenv("AMOUNT", "1.75"))
 
 # Cuenta de IQ Option: PRACTICE o REAL
-ACCOUNT_TYPE = os.getenv("ACCOUNT_TYPE", "PRACTICE").strip().upper()
+ACCOUNT_TYPE = os.getenv("ACCOUNT_TYPE", "REAL").strip().upper()
 
 # Límite total de entradas por ejecución del bot
-MAX_TOTAL_TRADES = 100
+MAX_TOTAL_TRADES = 2
 TOTAL_TRADES = 0
 CANDLE_COUNT = max(60, int(os.getenv("CANDLE_COUNT", "80")))
 MAX_OTC_PAIRS = max(1, int(os.getenv("MAX_OTC_PAIRS", "50")))
